b80e655d326b9f40bb5bde7a157aa84cf330224e
Noah Abdouabdounm@dukes.jmu.edu <abdounm@dukes.jmu.edu>
2020-12-12 19:50:58 -0500

    added name to schema

--- schema_no_enum.sql
+++ schema_no_enum.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS Programmers;
 CREATE TABLE Programmers (
     id SERIAL PRIMARY KEY,
+    full_name TEXT,
     gradyr INT DEFAULT 0,
     skills TEXT DEFAULT "web development",
     passions TEXT DEFAULT "saving the trees",
     langs TEXT DEFAULT "python",
-    skills TEXT DEFAULT "machine learning",
     experience TEXT DEFAULT "3 years of college",
     picture TEXT DEFAULT "www.minorityprogrammers.org/img/kush.jpg",
     is_deleted INT DEFAULT 0,
