01bd804f05091fe1395143c13457cdb387204b1f
Noah Abdouabdounm@dukes.jmu.edu <abdounm@dukes.jmu.edu>
2020-12-12 19:34:41 -0500

    change schema

---
+++ schema_no_enum.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS Programmers;
+CREATE TABLE Programmers (
+    id SERIAL PRIMARY KEY,
+    gradyr INT DEFAULT 0,
+    skills TEXT,
+    passions TEXT,
+    langs TEXT,
+    skills TEXT,
+    experience TEXT,
+    picture TEXT,
+    is_deleted INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
