7e66acfdc66c86cdf418be9d29b4adf90491bf16
Noah Abdouabdounm@dukes.jmu.edu <abdounm@dukes.jmu.edu>
2020-12-13 01:53:02 -0500

    fixing post with non hard coded params

--- app.js
+++ app.js
@@ -75,6 +75,18 @@ app.get('/Programmers/:gradyr', (request, response) => { //test this out
     });
   });
 }); // add more get statements, but be cognizant of appending long strings
+app.post('/Programmers/POST/:gradyr', (request, response) => {
+  const query = 'INSERT INTO Programmers(full_name, gradyr, skills, passions, langs, experience, picture) VALUES ("?", ?, "Web Development, Software Engineering","changing the world","Python, Chinese, Javascript, Java","3 years in college and 1 year in your woman","thispersondoesnotexist.com");';
+  // const params = [request.body.full_name, request.body.gradyr, request.body.skills, request.body.passions, request.body.langs , request.body.experience, request.body.picture]; // changed this to match  buildquote db
+  const params = [request.body.full_name, request.params.gradyr];
+  connection.query(query, params, (error, result) =>{
+    response.send({
+      ok: true,
+      id: result.insertID, // hopefull a provided function
+    });
+  });
+});
+
 
 app.post('/Programmers/POST/:gradyr', (request, response) => {
   const query = 'INSERT INTO Programmers(full_name, gradyr, skills, passions, langs, experience, picture) VALUES ("Tao Ren", ?, "Web Development, Software Engineering","changing the world","Python, Chinese, Javascript, Java","3 years in college and 1 year in your woman","thispersondoesnotexist.com");';
